6940d735d18c4bc2baac2b119fa9b99e601c261a
Sandeep Reddy <msande1996@gmail.com>
2016-11-01 00:29:15 +0530

    First commit

---
+++ app/src/main/java/com/harambe/devfetch/DevFetch.java
@@ -0,0 +1,15 @@
+package com.harambe.devfetch;
+
+import android.app.Application;
+
+/**
+ * Created by Sandeep on 01-11-2016.
+ */
+
+public class DevFetch extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
